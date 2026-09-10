ab1fb31d5e7a88e5ff042ef2bae8cb0504e2bf95
WilliWespe <willi.wespe@gmail.com>
2026-09-10 21:14:30 +0200

    .

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ require_once __DIR__ . '/php/translate.php';
     <button onclick="storeCalendarDataInRAM()">Fetch Data</button>
     <button onclick="addEventType('birthday', 'ffcc02')">Add event type</button>
     <button onclick="deleteEvent('6f70fa2e-ad45-11f1-834f-4c796e9137c8')">Delete event</button>
-    <button onclick="editEvent('8951bc98-ad47-11f1-834f-4c796e9137c8', 'Niklas Lochmann', '4abb804e-ad44-11f1-834f-4c796e9137c8', eventDotColor = null, includeEventInMail = 1)">Edit Event</button>
+    <button onclick="editEvent('8951bc98-ad47-11f1-834f-4c796e9137c8', 'Test', '4abb804e-ad44-11f1-834f-4c796e9137c8', eventDotColor = null, includeEventInMail = 1)">Edit Event</button>
     <button onclick="changeEventPosition('895cbbff-ad47-11f1-834f-4c796e9137c8', 10, 12, 1)">Change order</button>
     <?php 
         if (file_exists(__DIR__ . '/footer.php')) {
